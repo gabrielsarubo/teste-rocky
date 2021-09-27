a3631826a4744918b8b0e633601c148eb8527799
Gabriel <gabrielsarubo@hotmail.com>
2021-09-27 10:31:36 -0300

    Create side navigation menu

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("menu-sidenav").style.width = "220px"
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("menu-sidenav").style.width = "0"
+}
